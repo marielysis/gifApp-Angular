04d50b5d87971c94793ffd739d28f6900697b82f
Marielys Rodriguez <marielys11@yahoo.com>
2021-04-11 12:54:30 -0400

    Subiendo proyecyo a git

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private apiKey      : string = 'xdj1upXxTnQMezCpgydDBRGWVIkARAz8';
+  private servicioUrl : string = 'https://api.giphy.com/v1/gifs';
+  private _historial  : string[] = [];
+
+  // TODO: Cambiar any por su tipo
+  public resultados: Gif[] = [];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse( localStorage.getItem('historial')) || [];
+    this.resultados = JSON.parse( localStorage.getItem('resultados')) || [];
+    // if( localStorage.getItem('historial') ) {
+    //   this._historial = JSON.parse( localStorage.getItem('historial'));
+    // }
+  }
+
+  buscarGifs(query: string) {
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify( this._historial ) );
+    }
+
+    const params = new HttpParams()
+          .set('api_key', this.apiKey) 
+          .set('limit', '10') 
+          .set('q', query);
+
+    this.http
+      .get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params } )
+      .subscribe((resp: any) => {
+        // console.log( resp.data);
+        this.resultados = resp.data; 
+        localStorage.setItem('resultados', JSON.stringify( this.resultados ) );
+      });
+     
+  }
+}
